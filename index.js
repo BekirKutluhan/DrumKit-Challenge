fa582fb4807002a62c7c542cb402aa04f75ff0e3
Bekir Kutluhan <coskunkutluhanbekir@gmail.com>
2022-05-22 00:03:04 +0300

    Project Completed.

--- index.js
+++ index.js
@@ -1,39 +1,108 @@
-
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-document.addEventListener("keypress",function(){
+document.addEventListener("keypress", function() {
+  var buttonTriggered=event.key;
+  console.log(buttonTriggered);
+  switch (buttonTriggered) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+    document.querySelectorAll(".drum")[0].classList.add("pressed");
+      setTimeout(function() {document.querySelectorAll(".drum")[0].classList.remove("pressed")},200);
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      document.querySelectorAll(".drum")[1].classList.add("pressed");
+        setTimeout(function() {document.querySelectorAll(".drum")[1].classList.remove("pressed")}, 200);
+      break;
+    case "s":
+      tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      document.querySelectorAll(".drum")[2].classList.add("pressed");
+        setTimeout(function() {document.querySelectorAll(".drum")[2].classList.remove("pressed")}, 200);
+      break;
+    case "d":
+      tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      document.querySelectorAll(".drum")[3].classList.add("pressed");
+        setTimeout(function() {document.querySelectorAll(".drum")[3].classList.remove("pressed")}, 200);
+      break;
+    case "j":
+      snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      document.querySelectorAll(".drum")[4].classList.add("pressed");
+        setTimeout(function() {document.querySelectorAll(".drum")[4].classList.remove("pressed")}, 200);
+      break;
+    case "k":
+      crash = new Audio("sounds/crash.mp3"),
+        crash.play();
+      document.querySelectorAll(".drum")[5].classList.add("pressed");
+      setTimeout(function() {document.querySelectorAll(".drum")[5].classList.remove("pressed")}, 200);
+      break;
+    case "l":
+      kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      document.querySelectorAll(".drum")[6].classList.add("pressed");
+      setTimeout(function() {document.querySelectorAll(".drum")[6].classList.remove("pressed")}, 200);
+      break;
+    default:
+      console.log(buttonTriggered);
+  }
 
-    var buttonTriggered = event.key
-   switch (buttonTriggered) {
-     case  "w":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-       tom1.play();
-       break;
-        case "a":
-         var tom2 =new Audio("sounds/tom-2.mp3");
-         tom2.play();
-         break;
-         case "s":
-         tom3 =new Audio("sounds/tom-3.mp3");
-         tom3.play();
-         break;
-          case "d":
-         tom4=new Audio("sounds/tom-4.mp3");
-         tom4.play();
-         break;
-         case "j":
-          snare =new Audio("sounds/snare.mp3");
-         snare.play();
-         break;
-         case "k":
-        crash=new Audio("sounds/crash.mp3"),
-         crash.play();
-         break;
-         case "l":
-         kickBass=new Audio("sounds/kick-bass.mp3");
-         kickBass.play();
-         break;
-     default:console.log(buttonTriggered);
 
-   }
- });
+});
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+  var buttonClicked=event.target.innerHTML;
+  console.log(buttonClicked);
+  switch (buttonClicked) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+    document.querySelectorAll(".drum")[0].classList.add("pressed");
+      setTimeout(function() {document.querySelectorAll(".drum")[0].classList.remove("pressed")},200);
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      document.querySelectorAll(".drum")[1].classList.add("pressed");
+        setTimeout(function() {document.querySelectorAll(".drum")[1].classList.remove("pressed")}, 200);
+      break;
+    case "s":
+      tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      document.querySelectorAll(".drum")[2].classList.add("pressed");
+        setTimeout(function() {document.querySelectorAll(".drum")[2].classList.remove("pressed")}, 200);
+      break;
+    case "d":
+      tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      document.querySelectorAll(".drum")[3].classList.add("pressed");
+        setTimeout(function() {document.querySelectorAll(".drum")[3].classList.remove("pressed")}, 200);
+      break;
+    case "j":
+      snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      document.querySelectorAll(".drum")[4].classList.add("pressed");
+        setTimeout(function() {document.querySelectorAll(".drum")[4].classList.remove("pressed")}, 200);
+      break;
+    case "k":
+      crash = new Audio("sounds/crash.mp3"),
+        crash.play();
+      document.querySelectorAll(".drum")[5].classList.add("pressed");
+      setTimeout(function() {document.querySelectorAll(".drum")[5].classList.remove("pressed")}, 200);
+      break;
+    case "l":
+      kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      document.querySelectorAll(".drum")[6].classList.add("pressed");
+      setTimeout(function() {document.querySelectorAll(".drum")[6].classList.remove("pressed")}, 200);
+      break;
+    default:
+      console.log(buttonClicked);
+  }
+  });
+
+}
